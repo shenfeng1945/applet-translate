b7f7d6554f3991b7f94b22a42d03279b109cd960
shenfeng1945 <823902826@qq.com>
2023-02-08 21:36:49 +0800

    add language data

--- miniprogram/app.ts
+++ miniprogram/app.ts
@@ -1,18 +1,50 @@
 // app.ts
 App<IAppOption>({
-  globalData: {},
   onLaunch() {
     // 展示本地存储能力
-    const logs = wx.getStorageSync('logs') || []
-    logs.unshift(Date.now())
-    wx.setStorageSync('logs', logs)
-
-    // 登录
-    wx.login({
-      success: res => {
-        console.log(res.code)
-        // 发送 res.code 到后台换取 openId, sessionKey, unionId
-      },
-    })
   },
-})
+  globalData: {
+    curLang: {
+      lang: "en",
+      index: 0,
+      chinese: "英文",
+    },
+    langList: [
+      {
+        lang: "en",
+        index: 0,
+        chinese: "英文",
+      },
+      {
+        lang: "zh",
+        index: 1,
+        chinese: "中文",
+      },
+      {
+        lang: "jp",
+        index: 2,
+        chinese: "日语",
+      },
+      {
+        lang: "kor",
+        index: 3,
+        chinese: "韩语",
+      },
+      {
+        lang: "fra",
+        index: 4,
+        chinese: "法语",
+      },
+      {
+        lang: "spa",
+        index: 5,
+        chinese: "西班牙语",
+      },
+      {
+        lang: "ara",
+        index: 6,
+        chinese: "阿拉伯语",
+      },
+    ],
+  },
+});
